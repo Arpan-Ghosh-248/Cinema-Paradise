736aeb90a5322f252f9dbc12a2ebe129d97ae7b7
bablu <bablukumar@techis.io>
2022-10-19 14:21:01 +0530

    js added

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useDispatch } from 'react-redux';
+import IconLogo from '../../assets/img/icon-logo.svg';
+import { push } from 'connected-react-router';
+
+
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <img className="ft-lg" src={IconLogo} alt="" />
+                <p>Cinema Paradiso is the world’s leading community for creators to share, grow, and get hired.</p>
+                <h2>Contact</h2>
+                <p>E-mail : movie@cinemaparadiso.com | Hotline: +1 131 138 138</p>
+                <hr />
+                <p>CINEMA PARADISO - © 2022. ALL RIGHTS RESERVED.</p>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
