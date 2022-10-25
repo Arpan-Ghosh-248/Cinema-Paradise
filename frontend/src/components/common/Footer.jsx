9031e3429d05a8afb493917b09ad3611e5ff7f4e
bablu <bablukumar@techis.io>
2022-10-25 12:23:48 +0530

    Added common folder

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import IconLogo from '/Users/syedahmed/Desktop/cinema_paradiso/frontend/src/assets/img/Group 2218.png';
+
+
+
+
+
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <img src="{IconLogo}" alt="" />
+                <p>Cinema Paradiso is the world’s leading community for creators to share, grow, and get hired.</p>
+                <h2>Contact</h2>
+                <p>E-mail : bablukumar@techis.io | Hotline: +9177798777798</p>
+                <hr />
+                <p>CINEMA PARADISO - © 2022. ALL RIGHTS RESERVED.</p>
+            </footer>        
+        </>
+    );
+}
+
+export default Footer;
